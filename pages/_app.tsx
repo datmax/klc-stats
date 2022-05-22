97f941c9d7b3ce83c62ea6778a6624252f47e68e
datmax <max.datsun@student.univaq.it>
2022-05-22 04:12:24 +0200

    added gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
       <Component {...pageProps} />
+    </>
   )
 }
 
